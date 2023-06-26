7b1a74a0b012b8de5482003065cc98afb51ffc9a
Konrad Malik <konrad.malik@gmail.com>
2023-06-26 10:12:43 +0200

    feat: day 1 afternoon part 1

--- exercise/src/main.rs
+++ exercise/src/main.rs
@@ -1,33 +1,135 @@
-fn transpose(matrix: [[i32; 3]; 3]) -> [[i32; 3]; 3] {
-    let mut transposed = [[0; 3]; 3];
-    for ri in 0..3 {
-        for ci in 0..3 {
-            transposed[ri][ci] = matrix[ci][ri];
-        }
-    }
-    transposed
+use std::{fmt::Display, println};
+
+struct Library {
+    books: Vec<Book>,
 }
 
-fn pretty_print(matrix: &[[i32; 3]; 3]) {
-    for row in matrix {
-        for value in row {
-            print!("{} ", value)
+#[derive(Debug)]
+struct Book {
+    title: String,
+    year: u16,
+}
+
+impl Book {
+    // This is a constructor, used below.
+    fn new(title: &str, year: u16) -> Book {
+        Book {
+            title: String::from(title),
+            year,
         }
-        println!();
     }
 }
 
+impl Display for Book {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}, published in {}", self.title, self.year)
+    }
+}
+
+// Implement the methods below. Update the `self` parameter to
+// indicate the method's required level of ownership over the object:
+//
+// - `&self` for shared read-only access,
+// - `&mut self` for unique and mutable access,
+// - `self` for unique access by value.
+impl Library {
+    fn new() -> Library {
+        Library { books: Vec::new() }
+    }
+
+    fn len(&self) -> usize {
+        self.books.len()
+    }
+
+    fn is_empty(&self) -> bool {
+        self.books.is_empty()
+    }
+
+    fn add_book(&mut self, book: Book) {
+        self.books.push(book)
+    }
+
+    fn print_books(&self) {
+        for book in &self.books {
+            println!("{book}")
+        }
+    }
+
+    fn oldest_book(&self) -> Option<&Book> {
+        self.books.iter().min_by_key(|x| x.year)
+    }
+}
+
+// This shows the desired behavior. Uncomment the code below and
+// implement the missing methods. You will need to update the
+// method signatures, including the "self" parameter! You may
+// also need to update the variable bindings within main.
 fn main() {
-    let matrix = [
-        [101, 102, 103], // <-- the comment makes rustfmt add a newline
-        [201, 202, 203],
-        [301, 302, 303],
-    ];
+    let mut library = Library::new();
 
-    println!("matrix:");
-    pretty_print(&matrix);
+    println!("The library is empty: {}", library.is_empty());
 
-    let transposed = transpose(matrix);
-    println!("transposed:");
-    pretty_print(&transposed);
+    library.add_book(Book::new("Lord of the Rings", 1954));
+    library.add_book(Book::new("Alice's Adventures in Wonderland", 1865));
+
+    println!("The library is no longer empty: {}", library.is_empty());
+
+    library.print_books();
+
+    match library.oldest_book() {
+        Some(book) => println!("The oldest book is {}", book.title),
+        None => println!("The library is empty!"),
+    }
+
+    println!("The library has {} books", library.len());
+    library.print_books();
+}
+
+#[test]
+fn test_library_len() {
+    let mut library = Library::new();
+    assert_eq!(library.len(), 0);
+    assert!(library.is_empty());
+
+    library.add_book(Book::new("Lord of the Rings", 1954));
+    library.add_book(Book::new("Alice's Adventures in Wonderland", 1865));
+    assert_eq!(library.len(), 2);
+    assert!(!library.is_empty());
+}
+
+#[test]
+fn test_library_is_empty() {
+    let mut library = Library::new();
+    assert!(library.is_empty());
+
+    library.add_book(Book::new("Lord of the Rings", 1954));
+    assert!(!library.is_empty());
+}
+
+#[test]
+fn test_library_print_books() {
+    let mut library = Library::new();
+    library.add_book(Book::new("Lord of the Rings", 1954));
+    library.add_book(Book::new("Alice's Adventures in Wonderland", 1865));
+    // We could try and capture stdout, but let us just call the
+    // method to start with.
+    library.print_books();
+}
+
+#[test]
+fn test_library_oldest_book() {
+    let mut library = Library::new();
+    assert!(library.oldest_book().is_none());
+
+    library.add_book(Book::new("Lord of the Rings", 1954));
+    assert_eq!(
+        library.oldest_book().map(|b| b.title.as_str()),
+        Some("Lord of the Rings")
+    );
+
+    library.add_book(Book::new("Alice's Adventures in Wonderland", 1865));
+    assert_eq!(
+        library.oldest_book().map(|b| b.title.as_str()),
+        Some("Alice's Adventures in Wonderland")
+    );
 }
