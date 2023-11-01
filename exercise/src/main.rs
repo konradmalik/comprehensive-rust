ab7caeaedca2d0e07cf0accca63b236ed1b6dbf8
Konrad Malik <konrad.malik@gmail.com>
2023-11-01 15:03:29 +0100

    feat: day 3 morning exercise 1

--- exercise/src/main.rs
+++ exercise/src/main.rs
@@ -1,29 +1,106 @@
-trait NotEquals {
-    fn not_equals(&self, other: &Self) -> bool;
-}
+pub trait Widget {
+    /// Natural width of `self`.
+    fn width(&self) -> usize;
 
-trait Equals {
-    fn equals(&self, other: &Self) -> bool;
-}
+    /// Draw the widget into a buffer.
+    fn draw_into(&self, buffer: &mut dyn std::fmt::Write);
 
-impl<T> NotEquals for T where T: Equals {
-    fn not_equals(&self, other: &Self) -> bool {
-        !self.equals(other)
+    /// Draw the widget on standard output.
+    fn draw(&self) {
+        let mut buffer = String::new();
+        self.draw_into(&mut buffer);
+        println!("{buffer}");
     }
 }
 
-#[derive(Debug)]
-struct Centimeter(i16);
+pub struct Label {
+    label: String,
+}
 
-impl Equals for Centimeter {
-    fn equals(&self, other: &Centimeter) -> bool {
-        self.0 == other.0
+impl Label {
+    fn new(label: &str) -> Label {
+        Label {
+            label: label.to_owned(),
+        }
+    }
+}
+
+pub struct Button {
+    label: Label,
+}
+
+impl Button {
+    fn new(label: &str) -> Button {
+        Button {
+            label: Label::new(label),
+        }
+    }
+}
+
+pub struct Window {
+    title: String,
+    widgets: Vec<Box<dyn Widget>>,
+}
+
+impl Window {
+    fn new(title: &str) -> Window {
+        Window {
+            title: title.to_owned(),
+            widgets: Vec::new(),
+        }
+    }
+
+    fn add_widget(&mut self, widget: Box<dyn Widget>) {
+        self.widgets.push(widget);
+    }
+
+    fn inner_width(&self) -> usize {
+        std::cmp::max(
+            self.title.chars().count(),
+            self.widgets.iter().map(|w| w.width()).max().unwrap_or(0),
+        )
+    }
+}
+
+impl Widget for Label {
+    fn width(&self) -> usize {
+        self.label.len()
+    }
+
+    fn draw_into(&self, buffer: &mut dyn std::fmt::Write) {
+        writeln!(buffer, "{}", &self.label).unwrap()
+    }
+}
+
+impl Widget for Button {
+    fn width(&self) -> usize {
+        self.label.width() + "|  |".len()
+    }
+
+    fn draw_into(&self, buffer: &mut dyn std::fmt::Write) {
+        writeln!(buffer, "| {} |", &self.label.label).unwrap()
+    }
+}
+
+impl Widget for Window {
+    fn width(&self) -> usize {
+        let border_width = "=".len() * 6;
+        std::cmp::max(self.inner_width(), border_width)
+    }
+
+    fn draw_into(&self, buffer: &mut dyn std::fmt::Write) {
+        writeln!(buffer, "======").unwrap();
+        writeln!(buffer, "{}", &self.title).unwrap();
+        writeln!(buffer, "======").unwrap();
+        for widget in self.widgets.iter() {
+            widget.draw_into(buffer);
+        }
     }
 }
 
 fn main() {
-    let a = Centimeter(10);
-    let b = Centimeter(20);
-    println!("{a:?} equals {b:?}: {}", a.equals(&b));
-    println!("{a:?} not_equals {b:?}: {}", a.not_equals(&b));
+    let mut window = Window::new("Rust GUI Demo 1.23");
+    window.add_widget(Box::new(Label::new("This is a small text GUI demo.")));
+    window.add_widget(Box::new(Button::new("Click me!")));
+    window.draw();
 }
