078613b833c62242e215465c908235a0816c78df
Konrad Malik <konrad.malik@gmail.com>
2023-11-19 13:46:29 +0100

    feat: futures join

--- exercise/src/main.rs
+++ exercise/src/main.rs
@@ -1,27 +1,29 @@
-use tokio::sync::mpsc::{self, Receiver};
+use anyhow::Result;
+use futures::{future, join};
+use reqwest;
+use std::{collections::HashMap, time::Duration};
 
-async fn ping_handler(mut input: Receiver<()>) {
-    let mut count: usize = 0;
+async fn size_of_page(url: &str) -> Result<usize> {
+    let resp = reqwest::get(url).await?;
+    Ok(resp.text().await?.len())
+}
 
-    while let Some(_) = input.recv().await {
-        count += 1;
-        println!("Received {count} pings so far.");
-    }
-
-    println!("ping_handler complete");
+async fn just_sleep() -> Result<()> {
+    Ok(tokio::time::sleep(Duration::from_secs(2)).await)
 }
 
 #[tokio::main]
 async fn main() {
-    let (sender, receiver) = mpsc::channel(3);
-    let ping_handler_task = tokio::spawn(ping_handler(receiver));
-    for i in 0..10 {
-        sender.send(()).await.expect("Failed to send ping.");
-        println!("Sent {} pings so far.", i + 1);
-    }
-
-    drop(sender);
-    ping_handler_task
-        .await
-        .expect("Something went wrong in ping handler task.");
+    let urls: [&str; 4] = [
+        "https://google.com",
+        "https://httpbin.org/ip",
+        "https://play.rust-lang.org/",
+        "BAD_URL",
+    ];
+    let futures_iter = urls.into_iter().map(size_of_page);
+    let results = join!(future::join_all(futures_iter), just_sleep());
+    let page_sizes_dict: HashMap<&str, Result<usize>> =
+        urls.into_iter().zip(results.0.into_iter()).collect();
+    println!("slept {:?}", results.1);
+    println!("{:?}", page_sizes_dict);
 }
