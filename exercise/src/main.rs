45dba6d8c3738872e99f18a99729141fa6ce084a
Konrad Malik <konrad.malik@gmail.com>
2023-11-21 21:29:24 +0100

    feat: concurrency afternoon - async philosophers

--- exercise/src/main.rs
+++ exercise/src/main.rs
@@ -1,64 +1,43 @@
-use std::io::{self, ErrorKind};
-use std::time::Duration;
-use tokio::io::{AsyncReadExt, AsyncWriteExt, DuplexStream};
+use std::sync::Arc;
+use tokio::sync::mpsc::{self, Sender};
+use tokio::sync::Mutex;
+use tokio::time;
 
-struct LinesReader {
-    stream: DuplexStream,
-    bytes: Vec<u8>,
-    buf: [u8; 1],
+// Can you make your implementation single-threaded?
+
+struct Fork;
+
+struct Philosopher {
+    name: String,
+    // left_fork: ...
+    // right_fork: ...
+    // thoughts: ...
 }
 
-impl LinesReader {
-    fn new(stream: DuplexStream) -> Self {
-        Self {
-            stream,
-            bytes: Vec::new(),
-            buf: [0],
-        }
+impl Philosopher {
+    async fn think(&self) {
+        self.thoughts
+            .send(format!("Eureka! {} has a new idea!", &self.name))
+            .await
+            .unwrap();
     }
 
-    async fn next(&mut self) -> io::Result<Option<String>> {
-        while self.stream.read(&mut self.buf[..]).await? != 0 {
-            self.bytes.push(self.buf[0]);
-            if self.buf[0] == b'\n' {
-                break;
-            }
-        }
-        let raw = std::mem::take(&mut self.bytes);
-        if raw.is_empty() {
-            return Ok(None);
-        }
-        let s = String::from_utf8(raw)
-            .map_err(|_| io::Error::new(ErrorKind::InvalidData, "not UTF-8"))?;
-        Ok(Some(s))
+    async fn eat(&self) {
+        // Pick up forks...
+        println!("{} is eating...", &self.name);
+        time::sleep(time::Duration::from_millis(5)).await;
     }
 }
 
-async fn slow_copy(source: String, mut dest: DuplexStream) -> std::io::Result<()> {
-    for b in source.bytes() {
-        dest.write_u8(b).await?;
-        tokio::time::sleep(Duration::from_millis(10)).await
-    }
-    Ok(())
-}
+static PHILOSOPHERS: &[&str] = &["Socrates", "Hypatia", "Plato", "Aristotle", "Pythagoras"];
 
 #[tokio::main]
-async fn main() -> std::io::Result<()> {
-    let (client, server) = tokio::io::duplex(5);
-    let handle = tokio::spawn(slow_copy("hi\nthere\n".to_owned(), client));
+async fn main() {
+    // Create forks
 
-    let mut lines = LinesReader::new(server);
-    let mut interval = tokio::time::interval(Duration::from_millis(60));
-    loop {
-        tokio::select! {
-            _ = interval.tick() => println!("tick!"),
-            line = lines.next() => if let Some(l) = line? {
-                print!("{}", l)
-            } else {
-                break
-            },
-        }
-    }
-    handle.await.unwrap()?;
-    Ok(())
+    // Create philosophers
+
+    // Make them think and eat
+
+    // Output their thoughts
 }
